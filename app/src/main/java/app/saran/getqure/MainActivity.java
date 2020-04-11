8537814fb2930aac5a38933f7977c407a3957b17
saran pandiyan <pandiyansaran7@gmail.com>
2020-04-11 13:10:44 +0530

    if any doubt call me

--- app/src/main/java/app/saran/getqure/MainActivity.java
+++ app/src/main/java/app/saran/getqure/MainActivity.java
@@ -17,31 +17,10 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter adapter;
-
-    private List<itemView> listItems;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        listItems = new ArrayList<>();
-
-        for (int i =0;i<=10;i++) {
-            itemView itemView = new itemView("heading" + i + 1, "asdfghj jhgdss lkmvhcg lorem");
-
-            listItems.add(itemView);
-        }
-        adapter = new MyAdapter(listItems,this);
-
-        recyclerView.setAdapter(adapter);
 
     }
 }
